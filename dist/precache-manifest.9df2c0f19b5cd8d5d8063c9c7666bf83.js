5501045d77dfdde71524dd2a786079137afdad46
Jan Nonnen <jan.nonnen@viaboxx.de>
2020-07-31 13:10:44 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.9df2c0f19b5cd8d5d8063c9c7666bf83.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0514d5f4ab65c47d1b3e",
+    "url": "/css/app.5638acb7.css"
+  },
+  {
+    "revision": "d5e4a9a57a1aa38e636b",
+    "url": "/css/chunk-vendors.496dd37e.css"
+  },
+  {
+    "revision": "7362e27123e95e367970",
+    "url": "/css/details.8df9480d.css"
+  },
+  {
+    "revision": "894d5307045bc84cc351",
+    "url": "/css/search.07fb2808.css"
+  },
+  {
+    "revision": "8d669bc7767e049933936f636d67f550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0514d5f4ab65c47d1b3e",
+    "url": "/js/app.306c10cb.js"
+  },
+  {
+    "revision": "d5e4a9a57a1aa38e636b",
+    "url": "/js/chunk-vendors.e746d1c7.js"
+  },
+  {
+    "revision": "7362e27123e95e367970",
+    "url": "/js/details.aff1a4b1.js"
+  },
+  {
+    "revision": "0dd6ad642179ff1fc31e",
+    "url": "/js/home.9c6799d5.js"
+  },
+  {
+    "revision": "894d5307045bc84cc351",
+    "url": "/js/search.c50c19df.js"
+  },
+  {
+    "revision": "ecc2e9a2075f6a1b4c7be625498ee3c9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
